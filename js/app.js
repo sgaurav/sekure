5316d516cdd05780cc414781d0cecbb82af9658d
sgaurav <sgaurav.baghel@gmail.com>
2013-09-28 16:57:00 +0530

    added popup as message

--- js/app.js
+++ js/app.js
@@ -5,19 +5,25 @@ $(function(){
 	submitButton = jQuery('#saveDetailsButton'),
 	wrapper = jQuery('#wrapper');
 
-
+		//a dirty hack to animate collapsible
+	jQuery('#set1').on('expand', function () {
+	    jQuery(this).children().next().hide();
+	    jQuery(this).children().next().slideDown(200);
+	}).on('collapse', function () {
+   		jQuery(this).children().next().slideUp(200);
+	});
 
 	//TODO Load previously saved data
 
 
-	// Add new details
+		// Add new details
 	submitButton.on('click', function(e){
 		e.preventDefault();
 		$.publish('/addNew/', []);
 	});
 
 
-	//TODO Remove details
+	//TODO Delete details
 
 
 	//TODO Clear all
@@ -46,11 +52,14 @@ $(function(){
 
 			$.publish('/recreate/', []);
 
-			$.publish('/clear/', []);
-		} else {
+			$.publish('/clear/', [true]);
 
+			$.publish('/message/', ["Data successfully saved"]);
+		} 
+		else {
 			$.publish('/clear/', []);
-			//TODO put error message here
+
+			$.publish('/message/', ["Vehicle Number Missing."]);
 		}
 
 	});
@@ -59,20 +68,34 @@ $(function(){
 		//TODO
 	});
 
-	$.subscribe('/clear/', function(){
+	$.subscribe('/clear/', function(collapse){
 		jQuery('#journeyDetail').val('');
 		jQuery('#cabDetail').val('');
 		jQuery('#snapURI').val('');
 
-		jQuery( "#set1" ).trigger( "collapse" );
-
+		if(collapse){
+			jQuery( "#set1" ).trigger( "collapse" );
+		}
 	});
 
 	//TODO create a clear all
-	$.subscribe('/clearAll', function(){
+	$.subscribe('/clearAll/', function(){
 			//function to clear all images stored
 		localStorage.clear();
 		jQuery('.wrapper').empty();
+	});
+
+	$.subscribe('/message/', function(message){
+		var self = jQuery('#popupMessage');
+		var prepareMessage = '<p>' + message + '</p>';
+		self.html(prepareMessage);
+		
+		self.popup('open', {positionTo: 'window'});
+
+		var tempTimeout = setTimeout(function(){
+    			jQuery('#popupMessage').popup('close');
+    			clearTimeout(tempTimeout);
+			}, 2000);
 	});
 
 });
